d240bccdc7e8bf1598aaef08aa176317149744a8
Carlos A. Bonilla <lostineverland@gmail.com>
2018-02-14 16:43:36 -0500

    fleshout the date tools

--- pylambda/date_tools.py
+++ pylambda/date_tools.py
@@ -1,27 +1,301 @@
 import datetime
+from dateutil.relativedelta import relativedelta
+from utils import comp
+import objtypes
 
-ISO_8601_SECONDS = '%Y-%m-%dT%H:%M:%S'
-ISO_8601_MINUTES = '%Y-%m-%dT%H:%M'
-ISO_8601_HOURS = '%Y-%m-%dT%H'
-ISO_8601_DAYS = '%Y-%m-%d'
-ISO_8601_MONTHS = '%Y-%m'
-ISO_8601_YEARS = '%Y'
+# ISO_8601 string formats
+iso_8601 = objtypes.objDict(
+    MICROSECONDS='%Y-%m-%dT%H:%M:%S.%f',
+    SECONDS='%Y-%m-%dT%H:%M:%S',
+    MINUTES='%Y-%m-%dT%H:%M',
+    HOURS='%Y-%m-%dT%H',
+    DAYS='%Y-%m-%d',
+    MONTHS='%Y-%m',
+    YEARS='%Y',
+)
 
+granularity_by_len = {
+    26: 'MICROSECONDS',
+    19: 'SECONDS',
+    16: 'MINUTES',
+    13: 'HOURS',
+    10: 'DAYS',
+    7: 'MONTHS',
+    4: 'YEARS',
+}
 
-def datetime_to_ISO_second(dt):
-    return dt.strftime(ISO_8601_SECONDS)
+def get_iso_format(iso_dt):
+    return iso_8601[granularity_by_len.get(len(iso_dt), 'MICROSECONDS')]
 
-def datetime_to_ISO_minute(dt):
-    return dt.strftime(ISO_8601_MINUTES)
+class fromIsoToIso(object):
+    """a set of functions which take an ISO_8601 time string
+    and return an ISO_8601 time string
+    """
 
-def datetime_to_ISO_hour(dt):
-    return dt.strftime(ISO_8601_HOURS)
+    @staticmethod
+    def subtract_microseconds(microseconds, iso_dt):
+        granularity = granularity_by_len[len(iso_dt)].lower()
+        return comp(
+            fromIsoToDt.to_dt,
+            [fromDtToDt.subtract_microseconds, microseconds],
+            getattr(fromDtToIso, 'to_{}'.format(granularity))
+        )(iso_dt)
 
-def datetime_to_ISO_day(dt):
-    return dt.strftime(ISO_8601_DAYS)
+    @staticmethod
+    def subtract_seconds(seconds, iso_dt):
+        granularity = granularity_by_len[len(iso_dt)].lower()
+        return comp(
+            fromIsoToDt.to_dt,
+            [fromDtToDt.subtract_seconds, seconds],
+            getattr(fromDtToIso, 'to_{}'.format(granularity))
+        )(iso_dt)
 
-def datetime_to_ISO_month(dt):
-    return dt.strftime(ISO_8601_MONTHS)
+    @staticmethod
+    def subtract_minutes(minutes, iso_dt):
+        granularity = granularity_by_len[len(iso_dt)].lower()
+        return comp(
+            fromIsoToDt.to_dt,
+            [fromDtToDt.subtract_minutes, minutes],
+            getattr(fromDtToIso, 'to_{}'.format(granularity))
+        )(iso_dt)
 
-def datetime_to_ISO_year(dt):
-    return dt.strftime(ISO_8601_YEARS)
+    @staticmethod
+    def subtract_hours(hours, iso_dt):
+        granularity = granularity_by_len[len(iso_dt)].lower()
+        return comp(
+            fromIsoToDt.to_dt,
+            [fromDtToDt.subtract_hours, hours],
+            getattr(fromDtToIso, 'to_{}'.format(granularity))
+        )(iso_dt)
+
+    @staticmethod
+    def subtract_days(days, iso_dt):
+        granularity = granularity_by_len[len(iso_dt)].lower()
+        return comp(
+            fromIsoToDt.to_dt,
+            [fromDtToDt.subtract_days, days],
+            getattr(fromDtToIso, 'to_{}'.format(granularity))
+        )(iso_dt)
+
+    @staticmethod
+    def subtract_months(months, iso_dt):
+        granularity = granularity_by_len[len(iso_dt)].lower()
+        return comp(
+            fromIsoToDt.to_dt,
+            [fromDtToDt.subtract_months, months],
+            getattr(fromDtToIso, 'to_{}'.format(granularity))
+        )(iso_dt)
+
+    @staticmethod
+    def subtract_years(years, iso_dt):
+        granularity = granularity_by_len[len(iso_dt)].lower()
+        return comp(
+            fromIsoToDt.to_dt,
+            [fromDtToDt.subtract_years, years],
+            getattr(fromDtToIso, 'to_{}'.format(granularity))
+        )(iso_dt)
+
+    @staticmethod
+    def add_microseconds(microseconds, iso_dt):
+        granularity = granularity_by_len[len(iso_dt)].lower()
+        return comp(
+            fromIsoToDt.to_dt,
+            [fromDtToDt.add_microseconds, microseconds],
+            getattr(fromDtToIso, 'to_{}'.format(granularity))
+        )(iso_dt)
+
+    @staticmethod
+    def add_seconds(seconds, iso_dt):
+        granularity = granularity_by_len[len(iso_dt)].lower()
+        return comp(
+            fromIsoToDt.to_dt,
+            [fromDtToDt.add_seconds, seconds],
+            getattr(fromDtToIso, 'to_{}'.format(granularity))
+        )(iso_dt)
+
+    @staticmethod
+    def add_minutes(minutes, iso_dt):
+        granularity = granularity_by_len[len(iso_dt)].lower()
+        return comp(
+            fromIsoToDt.to_dt,
+            [fromDtToDt.add_minutes, minutes],
+            getattr(fromDtToIso, 'to_{}'.format(granularity))
+        )(iso_dt)
+
+    @staticmethod
+    def add_hours(hours, iso_dt):
+        granularity = granularity_by_len[len(iso_dt)].lower()
+        return comp(
+            fromIsoToDt.to_dt,
+            [fromDtToDt.add_hours, hours],
+            getattr(fromDtToIso, 'to_{}'.format(granularity))
+        )(iso_dt)
+
+    @staticmethod
+    def add_days(days, iso_dt):
+        granularity = granularity_by_len[len(iso_dt)].lower()
+        return comp(
+            fromIsoToDt.to_dt,
+            [fromDtToDt.add_days, days],
+            getattr(fromDtToIso, 'to_{}'.format(granularity))
+        )(iso_dt)
+
+    @staticmethod
+    def add_months(months, iso_dt):
+        granularity = granularity_by_len[len(iso_dt)].lower()
+        return comp(
+            fromIsoToDt.to_dt,
+            [fromDtToDt.add_months, months],
+            getattr(fromDtToIso, 'to_{}'.format(granularity))
+        )(iso_dt)
+
+    @staticmethod
+    def add_years(years, iso_dt):
+        granularity = granularity_by_len[len(iso_dt)].lower()
+        return comp(
+            fromIsoToDt.to_dt,
+            [fromDtToDt.add_years, years],
+            getattr(fromDtToIso, 'to_{}'.format(granularity))
+        )(iso_dt)
+
+class fromDtToDt(object):
+    """a set of functions which take a datetime object
+    and return a datetime object
+    """
+    @staticmethod
+    def subtract_microseconds(microseconds, dt):
+        delta = datetime.timedelta(microseconds=microseconds)
+        return dt - delta
+        
+    @staticmethod
+    def add_microseconds(microseconds, dt):
+        delta = datetime.timedelta(microseconds=microseconds)
+        return dt + delta
+        
+    @staticmethod
+    def subtract_seconds(seconds, dt):
+        delta = datetime.timedelta(seconds=seconds)
+        return dt - delta
+        
+    @staticmethod
+    def add_seconds(seconds, dt):
+        delta = datetime.timedelta(seconds=seconds)
+        return dt + delta
+        
+    @staticmethod
+    def subtract_minutes(minutes, dt):
+        delta = datetime.timedelta(seconds=60*minutes)
+        return dt - delta
+        
+    @staticmethod
+    def add_minutes(minutes, dt):
+        delta = datetime.timedelta(seconds=60*minutes)
+        return dt + delta
+        
+    @staticmethod
+    def subtract_hours(hours, dt):
+        delta = datetime.timedelta(seconds=3600*hours)
+        return dt - delta
+        
+    @staticmethod
+    def add_hours(hours, dt):
+        delta = datetime.timedelta(seconds=3600*hours)
+        return dt + delta
+        
+    @staticmethod
+    def subtract_days(days, dt):
+        delta = datetime.timedelta(days=days)
+        return dt - delta
+        
+    @staticmethod
+    def add_days(days, dt):
+        delta = datetime.timedelta(days=days)
+        return dt + delta
+        
+    @staticmethod
+    def subtract_months(months, dt):
+        delta = relativedelta(months=months)
+        return dt - delta
+        
+    @staticmethod
+    def add_months(months, dt):
+        delta = relativedelta(months=months)
+        return dt + delta
+        
+    @staticmethod
+    def subtract_years(years, dt):
+        delta = relativedelta(years=years)
+        return dt - delta
+        
+    @staticmethod
+    def add_years(years, dt):
+        delta = relativedelta(years=years)
+        return dt + delta
+        
+class fromIsoToDt(object):
+    """a set of functions which take an ISO_8601 time string
+    and return a datetime object
+    """
+    @staticmethod
+    def to_dt(iso_dt):
+        return datetime.datetime.strptime(iso_dt, get_iso_format(iso_dt))
+
+    @staticmethod
+    def from_microsecond(iso_dt):
+        return datetime.datetime.strptime(iso_dt, iso_8601.MICROSECONDS)
+
+    @staticmethod
+    def from_second(iso_dt):
+        return datetime.datetime.strptime(iso_dt, iso_8601.SECONDS)
+
+    @staticmethod
+    def from_minute(iso_dt):
+        return datetime.datetime.strptime(iso_dt, iso_8601.MINUTES)
+
+    @staticmethod
+    def from_hour(iso_dt):
+        return datetime.datetime.strptime(iso_dt, iso_8601.HOURS)
+
+    @staticmethod
+    def from_day(iso_dt):
+        return datetime.datetime.strptime(iso_dt, iso_8601.DAYS)
+
+    @staticmethod
+    def from_month(iso_dt):
+        return datetime.datetime.strptime(iso_dt, iso_8601.MONTHS)
+
+    @staticmethod
+    def from_year(iso_dt):
+        return datetime.datetime.strptime(iso_dt, iso_8601.YEARS)
+
+class fromDtToIso(object):
+    """a set of functions which take a datetime object
+    and return an ISO_8601 time string
+    """
+    @staticmethod
+    def to_microseconds(dt):
+        return dt.strftime(iso_8601.MICROSECONDS)
+
+    @staticmethod
+    def to_seconds(dt):
+        return dt.strftime(iso_8601.SECONDS)
+
+    @staticmethod
+    def to_minutes(dt):
+        return dt.strftime(iso_8601.MINUTES)
+
+    @staticmethod
+    def to_hours(dt):
+        return dt.strftime(iso_8601.HOURS)
+
+    @staticmethod
+    def to_days(dt):
+        return dt.strftime(iso_8601.DAYS)
+
+    @staticmethod
+    def to_months(dt):
+        return dt.strftime(iso_8601.MONTHS)
+
+    @staticmethod
+    def to_years(dt):
+        return dt.strftime(iso_8601.YEARS)
